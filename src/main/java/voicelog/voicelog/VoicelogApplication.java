a9a23551530a8417ca6ba02643ae927d54ecbd16
Sojin Lee <31687412+lsj90954511@users.noreply.github.com>
2024-05-18 22:05:49 +0900

    init: create project

---
+++ src/main/java/voicelog/voicelog/VoicelogApplication.java
@@ -0,0 +1,13 @@
+package voicelog.voicelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoicelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoicelogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/voicelog/voicelog/VoicelogApplicationTests.java
@@ -0,0 +1,13 @@
+package voicelog.voicelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoicelogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
