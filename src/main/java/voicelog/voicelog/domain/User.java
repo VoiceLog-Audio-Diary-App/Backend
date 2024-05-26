0a556c3c7b497b9581ab522eece15c95539b0241
Sojin Lee <31687412+lsj90954511@users.noreply.github.com>
2024-05-27 01:12:29 +0900

    feat: Add User Entity (#1)

---
+++ src/main/java/voicelog/voicelog/domain/User.java
@@ -0,0 +1,54 @@
+package voicelog.voicelog.domain;
+
+import jakarta.persistence.*;
+import voicelog.voicelog.dto.request.SignUpRequestDto;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(length = 50, nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer status;
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime created_at;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime updated_at;
+
+    @Column(nullable = false, length = 20)
+    private String type;
+
+    public User(SignUpRequestDto dto) {
+        this.username = dto.getEmail();
+        this.password = dto.getPassword();
+        this.name = dto.getName();
+        this.status = 1;
+        this.created_at = LocalDateTime.now();
+        this.updated_at = LocalDateTime.now();
+        this.type = "voicelog";
+    }
+}
